1a00d86425aaf9fb82d0fe30621eb1d3e2b90896
Noam <nopo1994@gmail.com>
2021-06-19 10:50:31 +0300

    change

--- app.py
+++ app.py
@@ -94,6 +94,8 @@ def sign_up():
             return jsonify({'message': 'password is too long, please select password up to 8 characters.'})
 
         user = User(username=username, password=password)
+        if not user:
+            return 'user already exist!'
         db.session.add(user)
         db.session.commit()
         return jsonify({'message': 'Welcome {}'.format(username)})
